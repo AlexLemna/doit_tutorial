1cf998301078df2375fcd4ce43be467e5c95f47e
Alexander Lemna <25907117+AlexLemna@users.noreply.github.com>
2022-02-03 23:50:50 +0000

    Part 3.1 (www.camillescott.org/pydoit-intermediate/03-dependencies.html)

--- dodo.py
+++ dodo.py
@@ -1,3 +1,6 @@
+from doit.tools import run_once
+
+
 def task_hello_world():
     return {"actions": ['echo "hello world!" > hello.txt'], "targets": ["hello.txt"]}
 
@@ -8,4 +11,5 @@ def task_download_data():
             "curl -OL https://s3.amazonaws.com/pydoit-intermediate/Melee_data.csv.gz"
         ],
         "targets": ["Melee_data.csv.gz"],
+        "uptodate": [run_once],
     }
