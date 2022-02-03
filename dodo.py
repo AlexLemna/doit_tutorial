1004739ed8a409f127c501171f9dcd02ec163784
Alexander Lemna <25907117+AlexLemna@users.noreply.github.com>
2022-02-03 23:45:24 +0000

    Part 2 (www.camillescott.org/pydoit-intermediate/02-targets.html)

--- dodo.py
+++ dodo.py
@@ -1,3 +1,11 @@
 def task_hello_world():
-    return {'actions': ['echo "hello world!" > hello.txt'],
-            'targets': ['hello.txt']}
+    return {"actions": ['echo "hello world!" > hello.txt'], "targets": ["hello.txt"]}
+
+
+def task_download_data():
+    return {
+        "actions": [
+            "curl -OL https://s3.amazonaws.com/pydoit-intermediate/Melee_data.csv.gz"
+        ],
+        "targets": ["Melee_data.csv.gz"],
+    }
